c7fa587fd615d938baee155712b735c75d612feb
wanggk <805119391@qq.com>
2023-02-17 22:58:36 +0800

    1.初始化框架

--- web-base/src/main/java/org/wgk/foolframework/base/BaseApplication.java
+++ web-base/src/main/java/org/wgk/foolframework/base/BaseApplication.java
@@ -8,7 +8,6 @@ public class BaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
-        System.out.println(21);
     }
 
 }
